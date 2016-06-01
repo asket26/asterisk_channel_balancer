2c523986688f5202f0f7dc981825cef82e04e965
asket26 <vetal.davv@gmail.com>
2016-06-01 14:05:04 +0300

    sql tune

--- balanced_call_channels.sql
+++ balanced_call_channels.sql
@@ -7,7 +7,7 @@ CREATE TABLE `balanced_call_channels` (
   `dstchannel` varchar(30) NOT NULL,
   `current-limit` bigint(20) DEFAULT '0',
   `current-state` tinyint(1) DEFAULT '0',
-  `day-limit` bigint(20) NOT NULL,
+  `day-limit` bigint(20) DEFAULT '0',
   `usage` bigint(20) DEFAULT '0',
   `enabled` tinyint(1) DEFAULT '1';
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
