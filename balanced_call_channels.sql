ef152c1df7981152ec3d052535bc9b8e4316e9f9
asket26 <vetal.davv@gmail.com>
2016-06-02 16:11:07 +0300

    add autorefill

--- balanced_call_channels.sql
+++ balanced_call_channels.sql
@@ -9,6 +9,7 @@ CREATE TABLE `balanced_call_channels` (
   `current-state` tinyint(1) DEFAULT '0',
   `day-limit` bigint(20) DEFAULT '0',
   `usage` bigint(20) DEFAULT '0',
-  `enabled` tinyint(1) DEFAULT '1';
+  `enabled` tinyint(1) DEFAULT '1',
+  `limitdate` date default '0';
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
